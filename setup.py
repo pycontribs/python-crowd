c6177296417b3711e0af521bdd6e1c5640a7b533
Alexander Else <aelse@else.id.au>
2012-06-04 12:58:32 +1000

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -12,4 +12,15 @@ setup(
     author = 'Alexander Else',
     author_email = 'aelse@else.id.au',
     url = 'https://github.com/aelse/python-crowd',
+
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
+    ]
 )
