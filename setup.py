6019299ae02967a53abe8a61d7fa508ece530e12
Alexander Else <aelse@else.id.au>
2016-03-26 15:26:14 +1100

    Bump to 1.0.0 before publishing, as this adds lxml dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='Crowd',
     license='BSD',
     py_modules=['crowd'],
-    version='0.9.1',
+    version='1.0.0',
     install_requires=['requests', 'lxml'],
 
     description='A python client to the Atlassian Crowd REST API',
